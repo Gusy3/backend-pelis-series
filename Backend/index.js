015e0621ae1ee955954849560820f9c52dc36545
Gusy <zyknos@gmail.com>
2022-11-02 11:36:44 +0100

    Archivos index.js y package actualizados

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,20 @@
 'use strict'
 
-var mongoose= require('mongoose');
-var app= require('./app');
+import mongoose from 'mongoose';
+import app from './app.js';
 var port= 3900;
 
-/* A partir de la version de Mongoose 6 esta opción no es necesaria */
-//mongoose.set('useFindAndModify', false);
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/pelis_series', {useNewUrlParser:true})
+    .then(()=>{
 
-mongoose.Promise= global.Promise;
-mongoose.connect('mongodb://localhost:27017/api_rest_blog',{useNewUrlParser:true}).then(()=>{
+        console.log("La conexión a la base de datos se ha realizado correctamente");
 
-    console.log('La conexión a la base de datos se ha realizado bien!!!');
+        // Crear servidor y ponerme a escuchar peticiones HTTP
+        app.listen(port,()=>{
 
-    /* Crear Servidor y ponerme a escuchar peticiones HTTP */
+            console.log("Servidor corriendo en http://localhost:"+port);
 
-    app.listen(port, ()=>{
-        
-        console.log('Servidor corriendo en http://localhost:'+port);
+        });
 
     });
-
-
-});
