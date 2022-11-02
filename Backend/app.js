83c18da1512f77faacb19ebfa552c89171e87e9f
Gusy <zyknos@gmail.com>
2022-11-02 11:35:08 +0100

    Archivo app.js creado

---
+++ Backend/app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// Cargar módulos de node para crear servidor
+import express from 'express';
+import bodyParser from 'body-parser';
+
+// Ejecutar express (http)
+var app = express();
+
+// Cargar ficheros rutas
+//import contact_routes from "./routes/contacts.js";
+
+//MiddLewares
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+// Añadir prefijos a rutas / Cargar rutas
+//app.use('/api', contact_routes);
+
+// Exportar el módulo (fichero actual)
+export default app;
