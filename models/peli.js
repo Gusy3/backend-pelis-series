6f77e30c17cc5ada5fec2a597eeca0add06bda01
Gusy <zyknos@gmail.com>
2022-11-28 16:46:30 +0100

    Modelo de pelis creado

---
+++ models/peli.js
@@ -0,0 +1,20 @@
+'use strict'
+
+import mongoose from "mongoose";
+var Schema = mongoose.Schema;
+
+var PeliSchema= Schema({
+    title: String,
+    gender: String,
+    year: Number,
+    resolution: String,
+    codec: String,
+    size: Number,
+    synopsis: String,
+    image: String,
+    created_at: {type: Date, default: Date.now}
+}, {
+    versionKey: false
+});
+
+export default mongoose.model('Pelis', PeliSchema);
